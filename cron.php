8e8dbe334f25c9939f6e1343a72c0d7e5c69c99b
iPaoo <ipaonp@gmail.com>
2020-04-13 20:00:08 -0300

    Atualizando

---
+++ cron.php
@@ -0,0 +1,8 @@
+<? php
+$ crontext = "Cron Run at" .date ("r"). "por". $ _ SERVER ['USER']. "\ n";
+$ folder = substr ($ _SERVER ['SCRIPT_FILENAME'], 0, strrpos ($_ SERVER ['SCRIPT_FILENAME'], "/") + 1);
+$ filename = $ pasta. "cron_test.txt";
+$ fp = fopen ($ nome do arquivo, "a") ou dado ("Erro de abertura!");
+fwrite ($ fp, $ crontext) ou die ("Erro de gravação!");
+fclose ($ fp);
+eco "Escreveu para". $ filename. "\ n \ n";
